c47c3facf645178b2ca6b91c5b23efea762ef688
andrewliebchen <andrewliebchen@gmail.com>
2015-02-13 20:21:11 -0800

    Parent drawing

--- daily-drawing.js
+++ daily-drawing.js
@@ -14,29 +14,12 @@ if (Meteor.isClient) {
     },
 
     cell: function() {
-      return Cells.find({});
+      return Cells.find({drawing: this._id});
     }
   });
 
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    Drawings.remove({});
-    Cells.remove({});
 
-    if(Drawings.find().count() === 0) {
-      Drawings.insert({});
-
-      Cells.insert({
-        x: 20,
-        y: 30
-      });
-
-      Cells.insert({
-        x: 25,
-        y: 30
-      });
-    }
-  });
 }
---
+++ seed.js
@@ -0,0 +1,34 @@
+if (Meteor.isServer) {
+
+  function parentDrawing(){
+    var parentDrawing = Drawings.insert({});
+    return parentDrawing;
+  }
+
+  Meteor.startup(function () {
+    Drawings.remove({});
+    Cells.remove({});
+
+    if(Drawings.find().count() === 0) {
+      var parentDrawingId = parentDrawing();
+
+      Cells.insert({
+        x: 20,
+        y: 30,
+        drawing: parentDrawingId
+      });
+
+      Cells.insert({
+        x: 25,
+        y: 30,
+        drawing: parentDrawingId
+      });
+
+      // Orphans
+      Cells.insert({
+        x: 50,
+        y: 50
+      });
+    }
+  });
+}
