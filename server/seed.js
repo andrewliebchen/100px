78ae6ff24d88ffd92528854c85aaa804230689bb
andrewliebchen <andrewliebchen@gmail.com>
2015-02-14 22:17:23 -0800

    Skinny layout

--- server/seed.js
+++ server/seed.js
@@ -38,6 +38,21 @@ if (Meteor.isServer) {
           "white", "white", "white", "white", "white", "white", "white", "white", "white", "white"
         ]
       });
+
+      Drawings.insert({
+        cells : [
+          "white", "white", "white", "white", "white", "white", "white", "white", "white", "white",
+          "white", "white", "black", "white", "white", "white", "white", "white", "white", "white",
+          "white", "white", "white", "white", "white", "white", "white", "white", "white", "white",
+          "white", "white", "white", "blue", "white", "white", "white", "white", "white", "white",
+          "white", "white", "white", "white", "white", "white", "white", "white", "white", "white",
+          "white", "white", "white", "white", "white", "white", "white", "white", "white", "white",
+          "white", "white", "white", "white", "white", "red", "white", "white", "white", "white",
+          "white", "white", "white", "white", "white", "white", "white", "white", "white", "white",
+          "white", "white", "white", "white", "white", "white", "white", "white", "white", "white",
+          "white", "white", "white", "white", "white", "white", "white", "white", "white", "white"
+        ]
+      });
     }
   });
 }
